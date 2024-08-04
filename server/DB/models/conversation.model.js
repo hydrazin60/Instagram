81d1ca2a3a62536d3ad4ea50f6214ebb63be8433
hydrazin60 <sureshkhdk45@gmail.com>
2024-08-04 10:04:18 +0545

    conversationsSchema , messageSchema create

---
+++ server/DB/models/conversation.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const conversationsSchema = new mongoose.Schema({
+  participants: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  messages: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+  ],
+});
+const Conversation = mongoose.model("Conversation", conversationsSchema);
+export default Conversation;
